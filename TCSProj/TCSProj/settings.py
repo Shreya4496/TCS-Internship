ba92ba5cc547e605085f303a1d557490e5303e80
shreya796 <shreya07.gupta@gmail.com>
2017-06-30 09:28:11 +0530

    settings.py

--- TCSProj/TCSProj/settings.py
+++ TCSProj/TCSProj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%&^-u#bi66*5uiw&d6_fk#k97007k$^uz!41qoh#i31%$oe%!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
