62bdd3acb3558b2e4d3400b6941eaa9a643e8aae
shreya4496 <shreyagupta4496@gmail.com>
2017-07-21 16:36:03 +0530

    URL updated

--- TCSProj/ChatBox/views.py
+++ TCSProj/ChatBox/views.py
@@ -106,7 +106,7 @@ def register(request):
 
 
 def send_email(toaddr,id):
-	text = "Hi!\nHow are you?\nHere is the link to activate your account:\nhttp://127.0.0.1:8000/register_activate/activation/?id=%s" %(id)
+	text = "Hi!\nHow are you?\nHere is the link to activate your account:\nhttp://shreya07.pythonanywhere.com/register_activate/activation/?id=%s" %(id)
 	# Record the MIME types of both parts - text/plain and text/html.
 	part1 = MIMEText(text, 'plain')
 	msg = MIMEMultipart('alternative')
